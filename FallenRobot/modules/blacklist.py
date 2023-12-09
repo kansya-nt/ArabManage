95e583b3cc3e9a40fb0ded0a6acc4a79a7d8e0c5
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-12-09 17:21:47 +0700

    Update blacklist.py
    Signed-off-by: sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>

--- FallenRobot/modules/blacklist.py
+++ FallenRobot/modules/blacklist.py
@@ -69,11 +69,13 @@ def blacklist(update, context):
 
 @user_admin
 @typing_action
+@user_admin
+@typing_action
 def add_blacklist(update, context):
     msg = update.effective_message
     chat = update.effective_chat
     user = update.effective_user
-    words = msg.text.split(None, 1)
+    reply_msg = msg.reply_to_message  # Ambil pesan yang di-reply
 
     conn = connected(context.bot, update, chat, user.id)
     if conn:
@@ -86,36 +88,41 @@ def add_blacklist(update, context):
         else:
             chat_name = chat.title
 
-    if len(words) > 1:
-        text = words[1]
-        to_blacklist = list(
-            {trigger.strip() for trigger in text.split("\n") if trigger.strip()}
-        )
-        for trigger in to_blacklist:
-            sql.add_to_blacklist(chat_id, trigger.lower())
-
-        if len(to_blacklist) == 1:
-            send_message(
-                update.effective_message,
-                "Added blacklist <code>{}</code> in chat: <b>{}</b>!".format(
-                    html.escape(to_blacklist[0]), html.escape(chat_name)
-                ),
-                parse_mode=ParseMode.HTML,
-            )
-
+    if reply_msg:  # Jika ada pesan yang di-reply
+        text = reply_msg.text
+    else:
+        words = msg.text.split(None, 1)
+        if len(words) > 1:
+            text = words[1]
         else:
             send_message(
                 update.effective_message,
-                "Added blacklist trigger: <code>{}</code> in <b>{}</b>!".format(
-                    len(to_blacklist), html.escape(chat_name)
-                ),
-                parse_mode=ParseMode.HTML,
+                "Tell me which words you would like to add in blacklist.",
             )
+            return
 
+    to_blacklist = list(
+        {trigger.strip() for trigger in text.split("\n") if trigger.strip()}
+    )
+    
+    for trigger in to_blacklist:
+        sql.add_to_blacklist(chat_id, trigger.lower())
+
+    if len(to_blacklist) == 1:
+        send_message(
+            update.effective_message,
+            "Added blacklist <code>{}</code> in chat: <b>{}</b>!".format(
+                html.escape(to_blacklist[0]), html.escape(chat_name)
+            ),
+            parse_mode=ParseMode.HTML,
+        )
     else:
         send_message(
             update.effective_message,
-            "Tell me which words you would like to add in blacklist.",
+            "Added blacklist trigger: <code>{}</code> in <b>{}</b>!".format(
+                len(to_blacklist), html.escape(chat_name)
+            ),
+            parse_mode=ParseMode.HTML,
         )
 
 
@@ -455,7 +462,7 @@ Blacklists are used to stop certain triggers from being said in a group. Any tim
  ❍ /blacklist*:* View the current blacklisted words.
 
 Admin only:
- ❍ /addblacklist <triggers>*:* Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
+ ❍ /addblacklist <triggers>*:* reply ke pesan atau tuliskan trigger yang ingin di blacklist di group.
  ❍ /unblacklist <triggers>*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
  ❍ /blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>*:* Action to perform when someone sends blacklisted words.
 """
