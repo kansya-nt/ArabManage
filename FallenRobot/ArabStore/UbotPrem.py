a4143cad502b1cc19853f9cb2b59ba8c11ae2ae6
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-12-13 11:53:23 +0700

    Update UbotPrem.py
    Signed-off-by: sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>

--- FallenRobot/ArabStore/UbotPrem.py
+++ FallenRobot/ArabStore/UbotPrem.py
@@ -1,1 +1,26 @@
+#Coded By: @Dhilnihnge
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+
+UbotPrem = """
+**ʜᴇʏ {message.from_user.mention},
+UBot Premium adalah Userbot dengan fitur yang di sesuaikan dan di permudah untuk masalah deployment.
+Dan Ubot Premium Support Segala Macam Emoji Premium di Dalam Modulnya
+
+• Bot Premium -> Rp.30k/bulan
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", user_id=OWNER_ID),
+                    InlineKeyboardButton(
+                        "Store",
+                        url="https://t.me/Arabc0de",
+                    ),
+                ]
+            ]
+        ),
+    )
+
 
