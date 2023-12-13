d5855a27f55652a236ee3a16e6a970e2954ff384
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-12-13 10:54:58 +0700

    Update ping.py
    Signed-off-by: sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>

--- FallenRobot/modules/ping.py
+++ FallenRobot/modules/ping.py
@@ -39,15 +39,15 @@ def ping(update: Update, context: CallbackContext):
     msg = update.effective_message
 
     start_time = time.time()
-    message = msg.reply_text("🏓 ᴘɪɴɢɪɴɢ....​")
+    message = msg.reply_text("🏓 Pingingg....​")
     end_time = time.time()
     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "ɪ ᴀᴍ ᴀʟɪᴠᴇ\n"
-        "<b>ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:</b> <code>{}</code>\n"
-        "<b>ᴜᴘᴛɪᴍᴇ:</b> <code>{}</code>".format(telegram_ping, uptime),
+        "Gua Masih Idup\n"
+        "<b>Masa Hidup:</b> <code>{}</code>\n"
+        "<b>Uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
     )
 
