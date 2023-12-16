81321a2b91ee14bce82d24aea58ecd2c338f69c6
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-12-17 04:31:55 +0700

    Create must_join.py

---
+++ FallenRobot/modules/must_join.py
@@ -0,0 +1,35 @@
+from FallenRobot import MUST_JOIN
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+
+from FallenRobot import pbot
+
+@pbot.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(bot: pbot, msg: Message):
+    if not MUST_JOIN:  # Not compulsory
+        return
+    try:
+        try:
+            await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
+        except UserNotParticipant:
+            if MUST_JOIN.isalpha():
+                link = "https://t.me/" + MUST_JOIN
+            else:
+                chat_info = await bot.get_chat(MUST_JOIN)
+                link = chat_info.invite_link
+            try:
+                await msg.reply(
+                    f"Join Dulu Kampang Baru Pencet /start Lagi !",
+                    disable_web_page_preview=True,
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("Masuk sini nyet, Jangan Lupa Salam", url=link)]
+                    ])
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"I'm not admin in the MUST_JOIN chat : {MUST_JOIN} !")
+
+  __mod_name__ = "mustjoin"
