12edb970514c6b1e4979f0fbacb61cd770858996
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-12-19 22:38:30 +0700

    Update source.py

--- FallenRobot/modules/source.py
+++ FallenRobot/modules/source.py
@@ -36,5 +36,103 @@ async def repo(_, message: Message):
         ),
     )
 
+@pbot.on_message(filters.command(["Store_Tele"]))
+async def Store_Tele(_, message: Message):
+    await message.reply_photo(
+        photo=START_IMG,
+        caption=f"""**Hai Mek {message.from_user.mention},
+
+Dibawah ini beberapa jasa bot dan jajanan telegram dari [SI ARAB STORE](https://t.me/Arabc0de)
+Silahkan Klik Button Di Bawah.**
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("ᴅʜɪʟ sɪ ᴧꝛᴧʙ", user_id=OWNER_ID),
+                ]
+                [
+                    InlineKeyboardButton(
+                        text="Ubot Premium",
+                        callback_data="Ubot_Prem",
+                    ),
+                    InlineKeyboardButton(
+                        text="Bot Telegram",
+                        callback_data="Bot_Tele",
+                    ),
+                ]
+            ]
+        ),
+    )
+
+@pbot.on_message(filters.command(["Ubot_Prem"]))
+async def Ubot_Prem(_, message: Message):
+    await message.reply_photo(
+        photo=START_IMG,
+        caption=f"""**Hai Mek {message.from_user.mention},
+
+Ubot Premium adalah userbot simple yang mmudahkan kalian tanpa harus melewati proses deploy yg rumit & dengan modul yang lebih keren serta full emoji premium jika akun anda premium,
+Untuk List Userbot Premium SI ARAB STORE bisa kalian cek list di bawah ini :**
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("ᴅʜɪʟ sɪ ᴧꝛᴧʙ", user_id=OWNER_ID),
+                ]
+                [
+                    InlineKeyboardButton(text="Ubot Spesial II", url="https://t.me/Spesial02Ubot",
+                    ),
+                    InlineKeyboardButton(text="Ubot Spesial III", url="https://t.me/Spesial03Ubot",
+                    ),
+                    InlineKeyboardButton(text="Ubot Spesial IV", url="https://t.me/Spesial04Ubot",
+                    ),
+                ]
+                [
+                    InlineKeyboardButton(text="Ubot Ultra I", url="https://t.me/ArabUltraUbot"),
+                    InlineKeyboardButton(text="Ubot Ultra II", url="https://t.me/Ultra02Ubot"),
+                    InlineKeyboardButton(text="Ubot Ultra III", url="https://t.me/Ultra03Ubot"),
+                ]
+                [
+                    InlineKeyboardButton(text="Kembali", callback_data="Store_Tele"),
+                ],
+            ]
+        ),
+    )
+
+@pbot.on_message(filters.command(["Bot_Tele"]))
+async def Bot_Tele(_, message: Message):
+    await message.reply_photo(
+        photo=START_IMG,
+        caption=f"""**Hai Mek {message.from_user.mention},
+
+Jasa Deploy Bot Telegram :
+
+• Userbot Gcast/Delayspam -> Rp.25K/bulan
+• Fsub/File Share/Bot Asupan -> Rp.30k/bulan(nambah button 10k)
+• Bot Musik Ram 4GB -> 100k/bulan
+• Bot Musik Ram 8GB -> 150k/bulan
+• Bot Manage -> 80k/bulan
+
+Info Selengkapnya Bisa Contact Saya. [ᴅʜɪʟ ᴧꝛᴧʙ](https://t.me/Dhilnihnge)**
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("ᴅʜɪʟ sɪ ᴧꝛᴧʙ", user_id=OWNER_ID),
+                ]
+                [
+                    InlineKeyboardButton(
+                        text="Ubot Premium",
+                        callback_data="Ubot_Prem",
+                    ),
+                ]
+                [
+                    InlineKeyboardButton(
+                        text="Kembali",
+                        callback_data="Store_Tele",
+                    ),
+                ]
+            ]
+        ),
+    )
 
 __mod_name__ = "Repo"
