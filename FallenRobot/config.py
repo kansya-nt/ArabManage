9357d1de67f376f62d44acc8013897b3782a0eff
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2023-12-25 05:24:09 +0700

    T config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -2,8 +2,8 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    API_ID = 16452568
-    API_HASH = "f936697c5c9e5bffd433babef7a4e4c9"
+    API_ID = 20609170
+    API_HASH = "cd0fa6d4f7c452ec5617bb0b1e4dc479"
 
     MUST_JOIN = "Berlinmusic_support"
 
@@ -11,18 +11,18 @@ class Config(object):
 
     DATABASE_URL = "postgres://zntgdebh:C_9TO4_Yaae7WAEMxRyyBzjqYQvwn1fA@pom.db.elephantsql.com/zntgdebh"  # A sql database url from elephantsql.com
 
-    EVENT_LOGS = (-1001795374467)  # Event logs channel to note down important bot level events
+    EVENT_LOGS = (-1002049500040)  # Event logs channel to note down important bot level events
 
     MONGO_DB_URI = "mongodb+srv://kansya:1234@db-mongodb-sgp1-52558-1312a8db.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-sgp1-52558"  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
-    START_IMG = "https://telegra.ph//file/a679b3ae99ff100437671.jpg"
+    START_IMG = "https://graph.org/file/163b8234f53e007e8f884.jpg"
 
     SUPPORT_CHAT = "Berlinmusic_support"  # Your Telegram support group chat username where your users will go and bother you
 
     TOKEN = "6742240257:AAHH7RDQILs6eHQFtdXwy02eLFsuNheQsx0"  # Get bot token from @BotFather on Telegram
 
-    TIME_API_KEY = "ZMOE8Q6BE25J7BEU"  # Get this value from https://timezonedb.com/api
+    TIME_API_KEY = "EYQXWAI8GT4M"  # Get this value from https://timezonedb.com/api
 
     OWNER_ID = 6024180996  # User id of your telegram account (Must be integer)
 
